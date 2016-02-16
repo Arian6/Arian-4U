7ef24b498d54235252c5a450baa1d3a61a351a68
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2016-02-16 13:43:54 -0500

    - SimpleEncyption done

--- ICS4U-ArianK/src/edu/hdsb/gwss/arian/u1/stringDataTypes/SimpleEncryption.java
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u1/stringDataTypes/SimpleEncryption.java
@@ -15,94 +15,10 @@ public class SimpleEncryption {
     static String encryption = "";
     static String decryption = "";
     static int userChosenRotation;
+    static ArrayList<String> alphabet;
 
     public static void main(String[] args) {
-        int decryptOrEncrypt;
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Encrypt / Decrypt a message!");
-        System.out.println("");
-
-        System.out.println("This program will allow the user to encrypt a message by rotation letters through the alphabet");
-        System.out.println("a certain nnumber of times (BETWEEN 1 - 25) chosen by the user");
-        System.out.println("E.g = If the user chooses 2, every A in the sentance becomes a C etc...");
-        System.out.println("This program will also allow you to ecrypt a message, by knowing the rotation an decyphering it");
-        System.out.println("");
-        System.out.println("Press 1 tp ENCRYPT a message, Press 2 to DECRYPT a message");
-        decryptOrEncrypt = input.nextInt();
-
-        if (decryptOrEncrypt == 1) {
-            userRotationNumberForEncryption();
-            if (userChosenRotation < 1 || userChosenRotation > 25) {
-
-                System.err.println("Invalid Rotation amount!");
-            } else if (userChosenRotation == 0) {
-                System.err.println("The letters wont change if rotated 0 times!");
-            } else {
-                userMessageToEncrypt();
-                encryptingAMessage();
-            }
-
-        } else if (decryptOrEncrypt == 2) {
-            userRotationNumberForDecryption();
-
-            if (userChosenRotation < 1 || userChosenRotation > 25) {
-
-                System.err.println("Invalid Rotation amount!");
-            } else if (userChosenRotation == 0) {
-                System.err.println("The letters wont change if rotated 0 times!");
-            } else {
-
-                userMessageToDecypher();
-                decryptingAMessage();
-            }
-
-        } else {
-            System.err.println("Not an Option!");
-        }
-    }
-
-    public static int userRotationNumberForEncryption() {
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter your rotation choice:");
-        userChosenRotation = input.nextInt();
-
-        return userChosenRotation;
-    }
-
-    public static int userRotationNumberForDecryption() {
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the number used to encrypt the message: ");
-        userChosenRotation = input.nextInt();
-
-        return userChosenRotation;
-    }
-
-    public static String userMessageToDecypher() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the message you want to decrypt:");
-        userChosenPhrase = input.nextLine();
-
-        return userChosenPhrase;
-
-    }
-
-    public static String userMessageToEncrypt() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the message you want to encrypt:");
-        userChosenPhrase = input.nextLine();
-
-        return userChosenPhrase;
-    }
-
-    public static void encryptingAMessage() {
-        ArrayList<String> alphabet = new ArrayList<String>();
         alphabet.add("A");
         alphabet.add("B");
         alphabet.add("C");
@@ -129,6 +45,73 @@ public class SimpleEncryption {
         alphabet.add("X");
         alphabet.add("Y");
         alphabet.add("Z");
+
+        int decryptOrEncrypt;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Encrypt / Decrypt a message!");
+        System.out.println("");
+
+        System.out.println("This program will allow the user to encrypt a message by rotation letters through the alphabet");
+        System.out.println("a certain nnumber of times (BETWEEN 1 - 25) chosen by the user");
+        System.out.println("E.g = If the user chooses 2, every A in the sentance becomes a C etc...");
+        System.out.println("This program will also allow you to ecrypt a message, by knowing the rotation an decyphering it");
+        System.out.println("");
+        System.out.println("Press 1 tp ENCRYPT a message, Press 2 to DECRYPT a message");
+        decryptOrEncrypt = input.nextInt();
+
+        if (decryptOrEncrypt == 1) {
+            userRotationNumber();
+            if (userChosenRotation < 1 || userChosenRotation > 25) {
+
+                System.err.println("Invalid Rotation amount!");
+            } else if (userChosenRotation == 0) {
+                System.err.println("The letters wont change if rotated 0 times!");
+            } else {
+                userMessageToDecypherOrEncrypt();
+                encryptingAMessage();
+            }
+
+        } else if (decryptOrEncrypt == 2) {
+            userRotationNumber();
+
+            if (userChosenRotation < 1 || userChosenRotation > 25) {
+
+                System.err.println("Invalid Rotation amount!");
+            } else if (userChosenRotation == 0) {
+                System.err.println("The letters wont change if rotated 0 times!");
+            } else {
+
+                userMessageToDecypherOrEncrypt();
+                decryptingAMessage();
+            }
+
+        } else {
+            System.err.println("Not an Option!");
+        }
+    }
+
+    public static int userRotationNumber() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your rotation choice:");
+        userChosenRotation = input.nextInt();
+
+        return userChosenRotation;
+    }
+
+    public static String userMessageToDecypherOrEncrypt() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the message you want to decrypt:");
+        userChosenPhrase = input.nextLine();
+
+        return userChosenPhrase;
+
+    }
+
+    public static void encryptingAMessage() {
 
         String conversion = userChosenPhrase;
 
@@ -160,33 +143,6 @@ public class SimpleEncryption {
     }
 
     public static void decryptingAMessage() {
-        ArrayList<String> alphabet = new ArrayList<String>();
-        alphabet.add("A");
-        alphabet.add("B");
-        alphabet.add("C");
-        alphabet.add("D");
-        alphabet.add("E");
-        alphabet.add("F");
-        alphabet.add("G");
-        alphabet.add("H");
-        alphabet.add("I");
-        alphabet.add("J");
-        alphabet.add("K");
-        alphabet.add("L");
-        alphabet.add("M");
-        alphabet.add("N");
-        alphabet.add("O");
-        alphabet.add("P");
-        alphabet.add("Q");
-        alphabet.add("R");
-        alphabet.add("S");
-        alphabet.add("T");
-        alphabet.add("U");
-        alphabet.add("V");
-        alphabet.add("W");
-        alphabet.add("X");
-        alphabet.add("Y");
-        alphabet.add("Z");
 
         String conversion = userChosenPhrase;
 

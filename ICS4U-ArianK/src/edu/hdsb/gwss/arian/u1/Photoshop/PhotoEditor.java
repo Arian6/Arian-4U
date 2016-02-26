0f6aa5f61cd9c22d9b3371eaa4202908a7296c49
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2016-02-26 12:39:22 -0500

    - phtooshop

---
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u1/Photoshop/PhotoEditor.java
@@ -0,0 +1,15 @@
+package edu.hdsb.gwss.arian.u1.Photoshop;
+
+import becker.xtras.imageTransformation.ITransformations;
+import becker.xtras.imageTransformation.ImageTransformerGUI;
+
+public class PhotoEditor extends Object {
+
+    public static void main( String args[] ) {
+
+        ITransformations trans = new Transformer();
+        ImageTransformerGUI theGUI = new ImageTransformerGUI( trans );
+
+    }
+
+}

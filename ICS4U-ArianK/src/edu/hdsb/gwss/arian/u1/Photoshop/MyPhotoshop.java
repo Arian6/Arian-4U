aa55780574638c1d80f7c4435554859c179524aa
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2016-03-21 14:40:48 -0400

    - recursion

--- ICS4U-ArianK/src/edu/hdsb/gwss/arian/u1/Photoshop/MyPhotoshop.java
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u1/Photoshop/MyPhotoshop.java
@@ -6,10 +6,7 @@ import becker.xtras.imageTransformation.ImageTransformerGUI;
 public class MyPhotoshop extends Object {
 
     public static void main(String args[]) {
-
         ITransformations trans = new Transformer();
         ImageTransformerGUI theGUI = new ImageTransformerGUI(trans);
-
     }
-
 }
---
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u2/recursion/FiveTerms.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.arian.u2.recursion;
+
+/**
+ *
+ * @author 1krasniqiari
+ */
+public class FiveTerms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int sequenceOne () {
+        
+        return;
+    }
+    
+    public static int sequenceTwo () {
+        
+        return;
+    }
+    
+}

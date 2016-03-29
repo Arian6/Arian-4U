c5ff65bae3f849d6aa27f085e54b849001ff8f55
ARIAN <ARIAN@192.168.2.11>
2016-03-29 17:38:27 -0400

    - five terms and pascals and letMeOut

--- ICS4U-ArianK/src/edu/hdsb/gwss/arian/u2/recursion/RecursiveFunctions.java
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u2/recursion/RecursiveFunctions.java
@@ -16,6 +16,40 @@ public class RecursiveFunctions {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("First Algorithm : f(x,y)");
+        System.out.println("f(5,3): " + firstFunction(5, 3));
+        System.out.println("f(2,2): " + firstFunction(2, 2));
+        System.out.println("f(1,4): " + firstFunction(1, 4));
+        System.out.println("f(-5,-2): " + firstFunction(-5, -2));
+
+        System.out.println("Ackermann's Function : a(m,n)");
+        System.out.println("a(1,1): " + ackermann(1, 1));
+        System.out.println("a(2,1): " + ackermann(2, 1));
+        System.out.println("a(5,5): " + ackermann(5, 5));
+
     }
-    
+
+    public static int firstFunction(int x, int y) {
+
+        if (x < y) {
+            return -firstFunction(y, x);
+        } else if (x == y) {
+            return 0;
+        } else {
+            return 1 + firstFunction(x - 1, y);
+        }
+
+    }
+
+    public static int ackermann(int m, int n) {
+
+        if (m == 0) {
+            return n + 1;
+        } else if (m > 0 && n == 0) {
+            return ackermann(m - 1, 1);
+        } else {
+            return ackermann(m - 1, ackermann(m, n - 1));
+        }
+
+    }
 }

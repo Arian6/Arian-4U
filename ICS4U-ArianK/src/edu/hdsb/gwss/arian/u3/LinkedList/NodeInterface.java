687877e5fe19167c40ffb16092cefeca29291fb2
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2016-05-06 14:20:00 -0400

    - linked interface started

---
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u3/LinkedList/NodeInterface.java
@@ -0,0 +1,23 @@
+package edu.hdsb.gwss.arian.u3.LinkedList;
+
+/**
+ * Lesson: 6.03 - a Node, in a Linked List
+ */
+public interface NodeInterface {
+
+    /**
+     * The node pointed to by 'next' is returned
+     */
+    public Node getNext();
+
+    /**
+     * The node pointed to by 'next' is changed to newNode
+     */
+    public void setNext( Node newNode );
+
+    /**
+     * The node pointed to by 'next' is returned.
+     */
+    public String getData();
+
+}

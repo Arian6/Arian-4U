00dc6e72541adf1c06008da5923430ee2b184ef8
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2016-05-24 13:25:07 -0400

    - plasdad

---
+++ ICS4U-ArianK/src/edu/hdsb/gwss/arian/u3/ikeaFurniture/Furniture.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.arian.u3.ikeaFurniture;
+
+
+
+/**
+ *
+ * @author 1krasniqiari
+ */
+public class Furniture {
+    
+    private String name;
+    private String typeOfFurniture;
+    private double netWorth;
+    private boolean monetized;
+    
+
+    
+    
+}
